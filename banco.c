ac4cf9aaa54f3aa4e96f4a6c0640f9fb414de332
Coerus <77758986+coerus@users.noreply.github.com>
2022-07-17 20:34:57 -0300

    haha

---
+++ banco.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+// code gore, pq sou um pessimo desenvolvedo :)
+int main (int argc, char *argv[])
+{
+   int escolha;
+   int deposito = 0;  
+   int saldo = 100;
+   int saque = 0;
+   printf("=============\n");
+   printf("bem vindo ao BB(Banco do Bostil)\n");
+   printf("=============\n");
+   printf("Digite [1] para sacar e [2] para depositar.\n");
+   printf("**Seu saldo inicial é: 100R$**\n");
+   scanf("%d", &escolha);
+   if (escolha == 2) {
+           
+      printf("Quanto deseja depositar?\n");
+      scanf("%d", &deposito);
+      saldo = saldo + deposito;
+      printf("Seu saldo é: %d\n", saldo);
+      }                     
+       if (escolha == 1) {
+      printf("Quanto deseja sacar?\n");
+      scanf("%d", &saque);
+      saldo = saldo - saque;
+      printf("Seu saldo é: %d\n", saldo);
+         
+      }
+              
+
+        
+    return 0;
+}
+
